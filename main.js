426836e177964db45b9a06a97b328ab390cdc061
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-11-03 15:42:14 -0300

    Refatorando codigo

--- main.js
+++ main.js
@@ -25,34 +25,24 @@ function createCard(date, day, games) {
   `
 }
 
-document.querySelector("#app").innerHTML = `
-  <header>
-    <img src="./assets/logo.svg" alt="" />
-  </header>
-  <main id="cards">
-    ${createCard(
-      "24/11",
-      "quinta",
-        createGame("switzerland", "07:00", "cameroon") +
-        createGame("uruguay", "10:00", "south-korea") +
-        createGame("portugal", "13:00", "ghana") +
-        createGame("brazil", "16:00", "serbia")
-    )}
-    ${createCard(
-      "28/11",
-      "segunda",
-        createGame("cameroon", "07:00", "serbia") +
-        createGame("south-korea", "10:00", "ghana") +
-        createGame("brazil", "13:00", "switzerland") +
-        createGame("portugal", "16:00", "uruguay")
-    )}
-    ${createCard(
-      "02/12",
-      "sexta",
-        createGame("south-korea", "12:00", "portugal") +
-        createGame("ghana", "12:00", "uruguay") +
-        createGame("serbia", "16:00", "switzerland") +
-        createGame("cameroon", "16:00", "brazil")
-    )}
-  </main>
-`
+document.querySelector("#cards").innerHTML =   
+    createCard("24/11", "quinta",
+      createGame("switzerland", "07:00", "cameroon") +
+      createGame("uruguay", "10:00", "south-korea") +
+      createGame("portugal", "13:00", "ghana") +
+      createGame("brazil", "16:00", "serbia")
+    ) + 
+
+    createCard("28/11", "segunda",
+      createGame("cameroon", "07:00", "serbia") +
+      createGame("south-korea", "10:00", "ghana") +
+      createGame("brazil", "13:00", "switzerland") +
+      createGame("portugal", "16:00", "uruguay")
+    ) + 
+
+    createCard("02/12", "sexta",
+      createGame("south-korea", "12:00", "portugal") +
+      createGame("ghana", "12:00", "uruguay") +
+      createGame("serbia", "16:00", "switzerland") +
+      createGame("cameroon", "16:00", "brazil")
+    )
